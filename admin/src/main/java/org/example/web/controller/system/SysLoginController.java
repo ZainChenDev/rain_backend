e0cedddbcf9c93802fcec0d4822ad5e95194a9dc
miacat <chen77078@gmail.com>
2025-06-05 23:39:07 +0800

    开始添加登录功能

--- admin/src/main/java/org/example/web/controller/system/SysLoginController.java
+++ admin/src/main/java/org/example/web/controller/system/SysLoginController.java
@@ -1,8 +1,13 @@
 package org.example.web.controller.system;
 
+import org.example.common.core.domain.CommonResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SysLoginController {
+    @PostMapping
+    public CommonResult<String> login(){
 
+    }
 }
--- framework/src/main/java/org/example/framework/web/service/SysLoginService.java
+++ framework/src/main/java/org/example/framework/web/service/SysLoginService.java
@@ -1,4 +1,15 @@
 package org.example.framework.web.service;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class SysLoginService {
+    public String login(String username, String password, String code, String uuid) {
+    }
+
+    public void loginPreCheck(String username, String password) {
+        // 登录前置校验逻辑 isBlank: 校验 null、长度为 0 和纯空白字符
+        if(StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            // TODO：抛出异常或返回错误信息
+        }
+    }
 }
