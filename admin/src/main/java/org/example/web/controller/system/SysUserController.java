09ab28f222dd65a5541a74419f7eb187a009216d
zainchen <chen77078@gmail.com>
2025-07-09 15:39:44 +0800

    添加User Permission权限获取业务

--- admin/src/main/java/org/example/web/controller/system/SysUserController.java
+++ admin/src/main/java/org/example/web/controller/system/SysUserController.java
@@ -1,19 +1,37 @@
 package org.example.web.controller.system;
 
+import org.example.common.core.domain.CommonResult;
 import org.example.common.core.domain.entity.SysUser;
+import org.example.common.core.domain.model.LoginUser;
+import org.example.common.util.SecurityUtils;
+import org.example.framework.web.dto.user.UserInfoResponseDTO;
+import org.example.framework.web.service.SysPermissionService;
 import org.example.system.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/system/user")
 public class SysUserController {
     @Autowired
     private ISysUserService userService;
+
+    @Autowired
+    private SysPermissionService permissionService;
+
+    @GetMapping("/info")
+    public CommonResult<UserInfoResponseDTO> getUserInfo() {
+        LoginUser loginUser = SecurityUtils.getLoginUser();
+        SysUser user = loginUser.getUser();
+        // 角色集合
+        Set<String> roles = permissionService.getRolePermission(user);
+        // 权限集合
+        Set<String> permissions = permissionService.getMenuPermission(user);
+        UserInfoResponseDTO userInfo = new UserInfoResponseDTO(user, roles, permissions);
+        return CommonResult.ok(userInfo);
+    }
 
 //    @GetMapping("/list")
 //    public List<SysUser> list() {
