1d1a564b174a0bad0b46aac4b13c1288d9e4e56b
zainchen <chen77078@gmail.com>
2025-07-09 15:37:33 +0800

    添加SysMenu基本逻辑

---
+++ system/src/main/java/org/example/system/service/ISysMenuService.java
@@ -0,0 +1,21 @@
+package org.example.system.service;
+
+import java.util.Set;
+
+public interface ISysMenuService {
+    /**
+     * 根据角色ID查询菜单树信息
+     *
+     * @param roleId 角色ID
+     * @return 选中菜单列表
+     */
+    Set<String> selectMenuPermsByRoleId(Long roleId);
+
+    /**
+     * 根据用户ID查询权限
+     *
+     * @param userId 用户ID
+     * @return 权限列表
+     */
+    Set<String> selectMenuPermsByUserId(Long userId);
+}
