65b76e9c8bd3c9165b204c6e17fae37698e3d63f
zainchen <chen77078@gmail.com>
2025-07-09 16:57:42 +0800

    添加RedisConfig，配置RedisTemplate并使用FastJson2序列化器

--- common/src/main/java/com/zainchen/common/constant/Constants.java
+++ common/src/main/java/com/zainchen/common/constant/Constants.java
@@ -22,4 +22,9 @@ public class Constants {
      * 令牌前缀
      */
     public static final String TOKEN_PREFIX = "Bearer ";
+
+    /**
+     * 自动识别json对象白名单配置（仅允许解析的包名，范围越小越安全）
+     */
+    public static final String[] JSON_WHITELIST_STR = { "org.springframework", "com.zainchen" };
 }
---
+++ framework/src/main/java/com/zainchen/framework/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.zainchen.framework.config;
+
+import com.zainchen.common.serializer.FastJson2JsonRedisSerializer;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+/**
+ * Redis配置类，配置RedisTemplate
+ */
+@Configuration
+public class RedisConfig implements CachingConfigurer {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // 使用FastJson2JsonRedisSerializer来序列化和反序列化redis的value值
+        FastJson2JsonRedisSerializer<Object> serializer = new FastJson2JsonRedisSerializer<>(Object.class);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
