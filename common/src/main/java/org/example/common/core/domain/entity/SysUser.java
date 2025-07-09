f73519653848fe2403f52807bc1a4cbb1ac7cf2a
zainchen <chen77078@gmail.com>
2025-07-09 15:45:43 +0800

    完善SysUser字段

--- common/src/main/java/org/example/common/core/domain/entity/SysUser.java
+++ common/src/main/java/org/example/common/core/domain/entity/SysUser.java
@@ -1,9 +1,12 @@
 package org.example.common.core.domain.entity;
 
+import java.io.Serial;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.example.common.constant.UserConstants;
 import org.example.common.core.domain.BaseEntity;
 
 /**
@@ -12,6 +15,9 @@ import org.example.common.core.domain.BaseEntity;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class SysUser extends BaseEntity {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     /**
      * 用户ID
      */
@@ -76,4 +82,28 @@ public class SysUser extends BaseEntity {
      * 最后登录时间
      */
     private LocalDateTime loginTime;
+
+    /**
+     * 密码最后更新时间
+     */
+    private LocalDateTime pwdUpdateTime;
+
+    /**
+     * 角色对象
+     */
+    private List<SysRole> roles;
+
+    /**
+     * 判断当前用户是否为管理员
+     */
+    public boolean isAdmin() {
+        return isAdmin(this.userId);
+    }
+
+    /**
+     * 判断当前用户是否为管理员 static方法
+     */
+    public static boolean isAdmin(Long userId) {
+        return UserConstants.ADMIN_USER_ID.equals(userId);
+    }
 }
