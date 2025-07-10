4de8582282c570034579b000b690f3e93dd954d0
miacat <chen77078@gmail.com>
2025-07-10 21:09:38 +0800

    完善jwt token filter

--- framework/src/main/java/com/zainchen/framework/security/filter/JwtAuthenticationTokenFilter.java
+++ framework/src/main/java/com/zainchen/framework/security/filter/JwtAuthenticationTokenFilter.java
@@ -1,17 +1,41 @@
 package com.zainchen.framework.security.filter;
 
+import com.zainchen.common.core.domain.model.LoginUser;
+import com.zainchen.common.util.SecurityUtils;
+import com.zainchen.framework.web.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * token过滤器 验证token有效性
+ */
+@Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
+    @Autowired
+    private TokenService tokenService;
 
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
+        LoginUser loginUser = tokenService.getLoginUser(request);
+        if (loginUser != null && SecurityUtils.getAuthentication() == null) {
+            tokenService.verifyToken(loginUser);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    loginUser, null, loginUser.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // 将认证信息存入 SecurityContext
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        chain.doFilter(request, response);
     }
 }
