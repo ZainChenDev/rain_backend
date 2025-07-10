c891a55987df49188a0f28ba2093683ae7c4de70
miacat <chen77078@gmail.com>
2025-07-10 21:17:28 +0800

    完善TokenService，使用jjwt 0.12.6 api

---
+++ common/src/main/java/com/zainchen/common/constant/CacheConstants.java
@@ -0,0 +1,8 @@
+package com.zainchen.common.constant;
+
+public class CacheConstants {
+    /**
+     * 登录用户 redis key
+     */
+    public static final String LOGIN_TOKEN_KEY = "login_tokens:";
+}
--- framework/src/main/java/com/zainchen/framework/web/service/TokenService.java
+++ framework/src/main/java/com/zainchen/framework/web/service/TokenService.java
@@ -1,7 +1,12 @@
 package com.zainchen.framework.web.service;
 
+import com.zainchen.common.constant.CacheConstants;
+import com.zainchen.common.core.redis.RedisClient;
 import eu.bitwalker.useragentutils.UserAgent;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecureDigestAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -9,12 +14,15 @@ import com.zainchen.common.constant.Constants;
 import com.zainchen.common.core.domain.model.LoginUser;
 import com.zainchen.common.util.ServletUtils;
 import com.zainchen.common.util.uuid.IdUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 @Slf4j
@@ -25,7 +33,7 @@ public class TokenService {
     @Value("${token.header}")
     private String header;
 
-    // 令牌秘钥
+    // 令牌秘钥 TODO 使用更好的方式存储秘钥
     @Value("${token.secret}")
     private String secret;
 
@@ -33,23 +41,44 @@ public class TokenService {
     @Value("${token.expire-time}")
     private int expireTime;
 
+    @Autowired
+    private RedisClient redisClient;
+
+    // JWT签名算法，使用HS256
+    private final static SecureDigestAlgorithm<SecretKey, SecretKey> ALGORITHM = Jwts.SIG.HS512;
+
+    // 秒转换为毫秒
     protected static final long MILLIS_SECOND = 1000;
 
+    // 分钟转换为毫秒
     protected static final long MILLIS_MINUTE = 60 * MILLIS_SECOND;
 
+    // 令牌刷新时间阈值，20分钟
     private static final Long MILLIS_MINUTE_TWENTY = 20 * 60 * 1000L;
 
-//    public LoginUser getLoginUser(HttpServletRequest request) {
-//        String token = getToken(request);
-//        if (StringUtils.isNotBlank(token)) {
-//            return null;
-//        }
-//    }
+    /**
+     * 从Http请求中获取令牌，由令牌中解析用户uuid，再从redis中获取登录信息
+     *
+     * @param request HttpServletRequest对象
+     * @return 登录信息
+     */
+    public LoginUser getLoginUser(HttpServletRequest request) {
+        String token = getToken(request);
+        if (StringUtils.isNotBlank(token)) {
+            Claims claims = parseToken(token);
+            // 解析对应的权限以及登录信息
+            String uuid = (String) claims.get(Constants.LOGIN_USER_KEY);
+            String userKey = getTokenKey(uuid);
+            return (LoginUser) redisClient.get(userKey);
+            //TODO 处理异常
+        }
+        return null;
+    }
 
     /**
      * 设置用户身份信息
      *
-     * @param loginUser 用户信息
+     * @param loginUser 登录信息
      */
     public void setUserAgent(LoginUser loginUser) {
         UserAgent userAgent = UserAgent.parseUserAgentString(ServletUtils.getRequest().getHeader("User-Agent"));
@@ -59,30 +88,51 @@ public class TokenService {
     }
 
     /**
-     * 创建令牌
+     * 创建令牌，包括uid、用户名<br>
+     * 设置用户代理信息，刷新令牌有效期（缓存到redis）<br>
      *
-     * @param loginUser 用户信息
+     * @param loginUser 登录信息
      * @return 生成的令牌
      */
     public String createToken(LoginUser loginUser) {
-        String token = IdUtils.fastUUID();
-        loginUser.setToken(token);
+        String uuid = IdUtils.fastUUID();
+        loginUser.setToken(uuid);
 
         setUserAgent(loginUser);
-//        refreshToken(loginUser);
+        refreshToken(loginUser);
 
         Map<String, Object> claims = new HashMap<>();
-        claims.put(Constants.LOGIN_USER_KEY, token);
+        claims.put(Constants.LOGIN_USER_KEY, uuid);
         claims.put(Constants.JWT_USERNAME, loginUser.getUsername());
         return createToken(claims);
     }
 
-//    public void refreshToken(LoginUser loginUser) {
-//        loginUser.setLoginTime(System.currentTimeMillis());
-//        loginUser.setExpireTime(System.currentTimeMillis() + expireTime * MILLIS_MINUTE);
-//
-//        String userKey = getLoginUserKey(loginUser.getToken());
-//    }
+    /**
+     * 验证令牌有效期，如果相差不足20分钟，自动刷新缓存
+     *
+     * @param loginUser 登录信息
+     */
+    public void verifyToken(LoginUser loginUser) {
+        long expireTime = loginUser.getExpireTime();
+        long currentTime = System.currentTimeMillis();
+        if (expireTime - currentTime <= MILLIS_MINUTE_TWENTY) {
+            refreshToken(loginUser);
+        }
+    }
+
+    /**
+     * 刷新令牌有效期，保存到redis中，key为带标识的uuid，value为登录用户信息<br>
+     * redis缓存的过期时间expireTime，单位为分钟TimeUnit.MINUTES
+     *
+     * @param loginUser 登录信息
+     */
+    public void refreshToken(LoginUser loginUser) {
+        loginUser.setLoginTime(System.currentTimeMillis());
+        loginUser.setExpireTime(System.currentTimeMillis() + expireTime * MILLIS_MINUTE);
+
+        String userKey = getTokenKey(loginUser.getToken());
+        redisClient.set(userKey, loginUser, expireTime, TimeUnit.MINUTES);
+    }
 
     /**
      * 通过自定义数据声明创建JWT令牌
@@ -91,11 +141,10 @@ public class TokenService {
      * @return JWT令牌
      */
     private String createToken(Map<String, Object> claims) {
-        SecretKey key = Jwts.SIG.HS512.key().build();
-
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         return Jwts.builder()
                 .claims(claims)
-                .signWith(key)
+                .signWith(key, ALGORITHM)
                 .compact();
     }
 
@@ -111,5 +160,30 @@ public class TokenService {
             token = token.replace(Constants.TOKEN_PREFIX, "");
         }
         return token;
+    }
+
+    /**
+     * 解析令牌，获取Claims对象
+     *
+     * @param token 加密的JWT令牌
+     * @return Claims对象，包含令牌中的自定义数据
+     */
+    private Claims parseToken(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    /**
+     * 将uuid拼装为登录用户的缓存key
+     *
+     * @param uuid 登录用户的唯一标识
+     * @return 登录用户的缓存key
+     */
+    private String getTokenKey(String uuid) {
+        return CacheConstants.LOGIN_TOKEN_KEY + uuid;
     }
 }
